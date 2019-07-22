cdcb03210841a89e4d4fec14ea7bdf7cba951186
Kenneth N <kngung99@gmail.com>
2019-07-22 15:36:58 -0700

    added base folder

---
+++ sprint-1/client/src/Nav.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+export default class Nav extends Component {
+  render() {
+    return (
+      <div>
+        <div className="logo">
+          <img />
+          <div />
+          <div>
+            <form>
+              <input />
+            </form>
+            <button>+ UPLOAD</button>
+            <img />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
